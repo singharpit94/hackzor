aee01866710cae2d29ac1ebf88a0d0619cc77ab0
ravehanker <ravehanker@7fd3e9e2-ca1a-0410-8b97-8755ef44912c>
2006-09-22 17:57:06 +0000

    First Code upload by ravehanker

---
+++ judge/evaluators/pyCode/add.py
@@ -0,0 +1,19 @@
+
+
+def isMatch(inFileName, outFileName):
+	inFile 	= open(inFileName, 'r')
+	outFile	= open(outFileName, 'r') 
+	
+	ins	 = inFile.read().strip().split('\n')
+	outs 	 = outFile.read().split('\n')
+
+	for i in range(len(ins)):
+		[ai, bi] = ins[i].split()
+		ci	 = outs[i]
+		if (int(ai) + int(bi) == int(ci)):
+			print ai, bi, ci, True
+		else:
+			print ai, bi, ci, False
+
+if __name__ == '__main__':
+	isMatch('../testCases/add.i', '../testCases/add.o')
