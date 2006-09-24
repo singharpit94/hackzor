2f52bbbeeb26ebb3ff54c4c664ca5250e3b46fb0
ravehanker <ravehanker@7fd3e9e2-ca1a-0410-8b97-8755ef44912c>
2006-09-24 09:52:06 +0000

    Commited Working, untested Build with templates added

---
+++ trunk/server/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls.defaults import *
+from hackzor.server.models import Question
+
+problem_dict = {
+	'queryset' : Question.objects.all()
+}
+
+urlpatterns = patterns('',
+
+		# To list the problem set
+    (r'^problems/$', 'django.views.generic.list_detail.object_list', dict(problem_dict, template_name='problem_set.html')),
+    		
+    		# To display a particular problem
+    (r'^problems/(?P<object_id>\d+)/$', 'django.views.generic.list_detail.object_detail', 
+								dict(problem_dict, template_name='view_problem.html')),
+    		
+    		# For now the problem set is the home page
+    (r'^$', 'django.views.generic.list_detail.object_list', dict(problem_dict, template_name='problem_set.html')),
+
+		# Auto-generated admin
+    (r'^admin/', include('django.contrib.admin.urls')),
+)
--- trunk/urls.py
+++ trunk/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls.defaults import *
+from django.views.static import serve
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^hackzor/', include('hackzor.apps.foo.urls.foo')),
-
-    # Uncomment this for admin:
-     (r'^admin/', include('django.contrib.admin.urls')),
+		# Everything we do will start with the URL opc/
+	(r'^opc/', include('hackzor.server.urls')),
+	# For serving static files. (like uploaded files etc.) For development Sever only
+	#(r'^(.*)$', 'django.views.static.serve', {'document_root': '/home/rave/Django/hackzor/'}),
 )
