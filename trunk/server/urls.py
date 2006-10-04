058a7d6163dd80f4da13b877df49bc9529e4a57f
ravehanker <ravehanker@7fd3e9e2-ca1a-0410-8b97-8755ef44912c>
2006-10-04 10:30:09 +0000

    Included better support for Viewing Problems

--- trunk/server/urls.py
+++ trunk/server/urls.py
@@ -11,8 +11,10 @@ urlpatterns = patterns('',
     (r'^problems/$', 'django.views.generic.list_detail.object_list', dict(problem_dict, template_name='problem_set.html')),
     		
     		# To display a particular problem
-    (r'^problems/(?P<object_id>\d+)/$', 'django.views.generic.list_detail.object_detail', 
-								dict(problem_dict, template_name='view_problem.html')),
+    #(r'^problems/(?P<object_id>\d+)/$', 'django.views.generic.list_detail.object_detail', 
+								#dict(problem_dict, template_name='view_problem.html')),
+    (r'^problems/(?P<id>\d+)/$', 'hackzor.server.views.viewProblem'), 
+	
     		
     		# For now the problem set is the home page
     (r'^$', 'django.views.generic.list_detail.object_list', dict(problem_dict, template_name='problem_set.html')),
--- trunk/server/views.py
+++ trunk/server/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from django.shortcuts import render_to_response, get_object_or_404
+from hackzor.server.models import Question
+
+def viewProblem (request, id):
+	object = Question.objects.get(id=id)
+	inp = open(object.testInput).read().split('\n')
+	out = open(object.testOutput).read().split('\n')
+	testCase = [x for x in zip(inp, out)]
+	return render_to_response('view_problem.html', {'object':object, 'testCase':testCase})
