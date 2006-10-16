0b44d5682f1d7d3f28ed3ae41ce61d6d109b7a33
prashmohan <prashmohan@7fd3e9e2-ca1a-0410-8b97-8755ef44912c>
2006-10-16 19:19:39 +0000

    Added generic Contest name and email support. Also made some formatting changes

--- server/models.py
+++ server/models.py
@@ -2,19 +2,16 @@ from django.contrib.auth.models import User
 from django.db import models
 import os
 
-
 class UserProfile(models.Model):
     """ Contains details about the user """
-    #TODO: Change this to self.name and have special method for activation key
+    # TODO: Change this to self.name and have special method for activation key
     def __str__(self):
-        return self.activation_key
+        return self.user.get_full_name()
     score = models.PositiveIntegerField(default=0)
     activation_key = models.CharField(maxlength=40)
     key_expires = models.DateTimeField() 
     user = models.OneToOneField(User)
     class Admin: pass
-
-
 
 class Question(models.Model):
     """ Contains the question content and path to evaluators and test cases """
@@ -34,7 +31,8 @@ class Attempt(models.Model):
     user = models.ForeignKey(UserProfile)
     question = models.ForeignKey(Question)
     result = models.BooleanField()
-    # Stores All submits made at the same time (to the precision of a second in the same path)
+    # Stores All submits made at the same time (to the precision of a second in
+    # the same path)
     # TODO:We will adjust precision as needed
     code_path = models.FileField(upload_to = r'submits/') 
     class Admin: pass
