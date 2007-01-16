063d1a174db878bf72f0526846bebf7aaf32fbd9
prashmohan <prashmohan@7fd3e9e2-ca1a-0410-8b97-8755ef44912c>
2007-01-16 13:21:38 +0000

    * Merged fixes done for Kurukhetra OPC * Added Support for new evaluators   ** C   ** C++   ** Java   ** Python   ** Ruby   ** Perl   ** PHP   ** C Sharp * Added Support for additional RunTime/Memory based on Language * Scoring logic pushed back to server. Removed score function

--- evaluator/exec.py
+++ evaluator/exec.py
@@ -3,25 +3,11 @@
 import sys
 import resource
 import os
-import commands
 mem_limit = int(sys.argv[1])
 resource.setrlimit(resource.RLIMIT_AS, (mem_limit, mem_limit))
+resource.setrlimit(resource.RLIMIT_NPROC, (0,0))
 # TODO: Correct the Maximum No of open Files! Currently not working
 no_of_files = 3
 #resource.setrlimit(resource.RLIMIT_NOFILE, (no_of_files, no_of_files))
 cmd = sys.argv[2]
-
-#args = ''
-# try:
-    
-#     #sys.exit()
-# except:
-#     for x in sys.exc_info():
-#         print x
-#     sys.exit(1)
-
-#os.system(os.path.join(os.getcwd(), cmd))
-#sys.exit(os.system(cmd))
-#status, op = commands.getstatusoutput(cmd)
-os.execv(cmd,[])
-print op
+os.execv(cmd,[os.path.basename(cmd)])
